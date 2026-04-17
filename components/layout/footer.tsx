1032ecef0f250e9d722b43e3899c2fcd80c5177a
Mohamed-Irfan-git <irfanmt29@gmail.com>
2026-04-17 23:36:36 +0530

    lets see

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer className="mt-10 border-t border-white/10 bg-slate-900 text-center text-sm text-slate-400">
       <div className="mx-auto max-w-7xl px-6 py-6">
-        {new Date().getFullYear()} IrfanStore. All rights reserved.
+        {/* © {new Date().getFullYear()}  */}
+        IrfanStore. All rights reserved.
       </div>
     </footer>
   );
